753acdcf74eb718a47f507c14cfefe452d2cce12
maxjay <maxjay2000@gmail.com>
2022-10-31 16:23:28 +0000

    event creation screen with local store

--- lib/screens/createEventScreen.dart
+++ lib/screens/createEventScreen.dart
@@ -6,6 +6,7 @@ import 'package:flutter_login/flutter_login.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:date_time_picker/date_time_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sossi_app/model/Event.dart';
 
 import '../constants.dart';
@@ -28,95 +29,143 @@ class _CreateEventScreenState extends State<CreateEventScreen> {
   String endDateString = "";
   TextEditingController eventNameController = TextEditingController();
   TextEditingController eventDescController = TextEditingController();
+  TextEditingController eventLocationController = TextEditingController();
   TextEditingController startDateController =
       TextEditingController(text: DateTime.now().toString());
   TextEditingController endDateController =
       TextEditingController(text: DateTime.now().toString());
 
+  Future<void> saveEventLocally(event) async {
+    print("saving");
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? eventListString = prefs.getString('events');
+    List<Event> events = Event.decode(eventListString!);
+    events.add(event);
+    print(events);
+    await prefs.setString("events", Event.encode(events));
+    prefs.reload();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.lightGreen[50],
-        resizeToAvoidBottomInset: false,
-        body: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              TextFormField(
-                controller: eventNameController,
-                decoration: const InputDecoration(
-                  hintText: 'Event Name',
+        backgroundColor: Colors.grey[100],
+        resizeToAvoidBottomInset: true,
+        body: Center(
+          child: Builder(builder: (context) {
+            return Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Card(
+                color: Colors.white,
+                child: Form(
+                  key: _formKey,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            controller: eventNameController,
+                            decoration: const InputDecoration(
+                              hintText: 'Event Name',
+                            ),
+                            validator: (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                          TextFormField(
+                            controller: eventDescController,
+                            decoration: const InputDecoration(
+                              hintText: 'Event Description',
+                            ),
+                            validator: (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                          DateTimePicker(
+                              type: DateTimePickerType.dateTime,
+                              initialValue: DateTime.now().toString(),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2100),
+                              // controller: startDateController,
+                              icon: Icon(Icons.event),
+                              cursorColor: Colors.red,
+                              dateLabelText: 'Start Date for Event',
+                              validator: (val) {
+                                setState(() => startDateString = val ?? '');
+                                if (val == null || val.isEmpty) {
+                                  return "Please enter a start date";
+                                }
+                                return null;
+                              }),
+                          DateTimePicker(
+                              type: DateTimePickerType.dateTime,
+                              initialValue: DateTime.now().toString(),
+                              icon: Icon(Icons.event),
+                              firstDate: DateTime(2000),
+                              lastDate: DateTime(2100),
+                              // controller: endDateController,
+                              dateLabelText: 'End Date for Event',
+                              validator: (val) {
+                                setState(() => endDateString = val ?? '');
+                                if (val == null || val.isEmpty) {
+                                  return "Please enter a stop date";
+                                }
+                                return null;
+                              }),
+                          TextFormField(
+                            controller: eventLocationController,
+                            decoration: const InputDecoration(
+                                hintText: 'Event Location',
+                                icon: Icon(Icons.add_location)),
+                            validator: (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 16.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  // Send it off
+                                  saveEventLocally(Event(
+                                      name: eventNameController.text,
+                                      description: eventDescController.text,
+                                      dateStart:
+                                          DateTime.parse(startDateString),
+                                      dateEnd: DateTime.parse(endDateString),
+                                      orgID: '',
+                                      orgName: '',
+                                      rating: 5,
+                                      rsvpNum: 0));
+                                  Constants.writeEvents();
+                                  Navigator.pop(context);
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(
+                                        content: Text('Adding Event')),
+                                  );
+                                }
+                              },
+                              child: const Text('Submit'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
               ),
-              TextFormField(
-                controller: eventDescController,
-                decoration: const InputDecoration(
-                  hintText: 'Event Description',
-                ),
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              DateTimePicker(
-                  type: DateTimePickerType.dateTime,
-                  initialValue: DateTime.now().toString(),
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2100),
-                  // controller: startDateController,
-                  icon: Icon(Icons.event),
-                  dateLabelText: 'Start Date for Event',
-                  validator: (val) {
-                    setState(() => startDateString = val ?? '');
-                    if (val == null || val.isEmpty) {
-                      return "Please enter a start date";
-                    }
-                    return null;
-                  }),
-              DateTimePicker(
-                  type: DateTimePickerType.dateTime,
-                  initialValue: DateTime.now().toString(),
-                  icon: Icon(Icons.event),
-                  firstDate: DateTime(2000),
-                  lastDate: DateTime(2100),
-                  // controller: endDateController,
-                  dateLabelText: 'End Date for Event',
-                  validator: (val) {
-                    setState(() => endDateString = val ?? '');
-                    if (val == null || val.isEmpty) {
-                      return "Please enter a stop date";
-                    }
-                    return null;
-                  }),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      Event(
-                          name: eventNameController.text,
-                          description: eventDescController.text,
-                          dateStart: DateTime.parse(startDateString),
-                          dateEnd: DateTime.parse(endDateString));
-                      //Send it off
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                    }
-                  },
-                  child: const Text('Submit'),
-                ),
-              ),
-            ],
-          ),
+            );
+          }),
         ));
   }
 }
--- lib/screens/eventList.dart
+++ lib/screens/eventList.dart
@@ -9,32 +9,44 @@ import '../constants.dart';
 import '../model/Event.dart';
 
 import "package:sossi_app/constants.dart";
-
-List<Event> fetchEvents(fun) {
-  fun();
-  return Constants.events;
-}
+import 'package:shared_preferences/shared_preferences.dart';
 
 class EventList extends StatefulWidget {
   const EventList({Key? key}) : super(key: key);
-
   @override
   // ignore: library_private_types_in_public_api
   _EventListState createState() => _EventListState();
 }
 
 class _EventListState extends State<EventList> {
-  late final List<Event> events;
+  late List<Event> events = Constants.events;
   late bool isLoading = true;
 
+  Future<void> fetchEvents() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.reload();
+    await prefs.setString("events", Event.encode(events));
+    final String? eventListString = prefs.getString('events');
+    if (eventListString != null) {
+      setState(() {
+        events = Event.decode(eventListString!);
+      });
+    }
+    loaded();
+  }
+
   void loaded() {
-    isLoading = false;
+    setState(() {
+      isLoading = false;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    events = fetchEvents(loaded);
+    List<Event> events = Constants.events;
+    fetchEvents();
+    isLoading = true;
   }
 
   @override
@@ -50,26 +62,26 @@ class _EventListState extends State<EventList> {
       backgroundColor: Colors.grey[100],
       resizeToAvoidBottomInset: false,
       body: ListView.builder(
-        itemCount: Constants.events.length,
+        itemCount: events.length,
         padding: const EdgeInsets.all(12.0),
         itemBuilder: (BuildContext c, int index) {
           return GestureDetector(
               onTap: () {
                 Navigator.of(context).pushNamed('/detailEvent',
                     arguments: Event(
-                        name: Constants.events[index].name,
-                        description: Constants.events[index].description,
-                        date: Constants.events[index].date,
-                        latitude: Constants.events[index].latitude,
-                        longitude: Constants.events[index].longitude,
-                        dateStart: Constants.events[index].dateStart,
-                        dateEnd: Constants.events[index].dateEnd,
-                        orgID: Constants.events[index].orgID,
-                        orgName: Constants.events[index].orgName,
-                        rsvpNum: Constants.events[index].rsvpNum,
-                        rating: Constants.events[index].rating,
-                        eventID: Constants.events[index].eventID,
-                        isOngoing: Constants.events[index].isOngoing));
+                        name: events[index].name,
+                        description: events[index].description,
+                        date: events[index].date,
+                        latitude: events[index].latitude,
+                        longitude: events[index].longitude,
+                        dateStart: events[index].dateStart,
+                        dateEnd: events[index].dateEnd,
+                        orgID: events[index].orgID,
+                        orgName: events[index].orgName,
+                        rsvpNum: events[index].rsvpNum,
+                        rating: events[index].rating,
+                        eventID: events[index].eventID,
+                        isOngoing: events[index].isOngoing));
               },
               child: Card(
                   child: Column(children: [
@@ -78,9 +90,9 @@ class _EventListState extends State<EventList> {
                     Icons.event_note,
                     size: 30,
                   ),
-                  title: Text(Constants.events[index].name),
-                  subtitle: Text(Constants.events[index].orgName),
-                  trailing: Constants.events[index].isOngoing
+                  title: Text(events[index].name),
+                  subtitle: Text(events[index].orgName),
+                  trailing: events[index].isOngoing
                       ? Icon(
                           Icons.event_available,
                           color: Theme.of(context).primaryColor,
@@ -96,7 +108,10 @@ class _EventListState extends State<EventList> {
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-          Navigator.of(context).pushNamed("/createEvent");
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const CreateEventScreen()),
+          ).then((value) => fetchEvents());
         },
       ),
     );
